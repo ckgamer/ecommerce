4c3ce1b533974f8f85781f87c042646a1361c7d4
Ganesh <ganesh@webenertia.com>
2023-10-27 14:02:37 +0530

    initial

---
+++ app/api/route.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+import stringify from 'json-stringify-safe';
+
+export async function GET(request) {
+  // // Replace the uri string with your connection string.
+  const uri = "mongodb+srv://ckgamer:Xu4U1mQ6Rg2YQQwQ@cluster0.okyilff.mongodb.net/";
+  const client = new MongoClient(uri);
+  
+  try {
+      const database = client.db("productDb");
+      const movies = database.collection("products");
+    
+      //     // Query for a movie that has the title 'Back to the Future'
+      const query = {};
+          const movie = await movies.findOne(query);
+          const content = stringify(movie);
+          // console.log(request);
+          console.log(movie);
+      return NextResponse.json({ a: 34,content});
+
+
+    } finally {
+  //     // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+}
